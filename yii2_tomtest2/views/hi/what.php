a5ab7567d6cea63066af82968c2eba178a99a6df
tommykung <tommykung007@gmail.com>
2017-12-05 18:48:00 +0700

    //test4

--- yii2_tomtest2/views/hi/what.php
+++ yii2_tomtest2/views/hi/what.php
@@ -1,7 +1,7 @@
 <?php $this->title = 'My Yii Application'; ?>
 <div class="site-index">
     <div class="jumbotron">
-        <h1>What Content test</h1>
+        <h1>What Content test crub</h1>
         <p class="lead">What Your Name</p>
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Noppadol</a></p>
     </div>

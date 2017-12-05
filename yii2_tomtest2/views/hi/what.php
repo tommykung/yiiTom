db5e03e0802b46da799ede74e7a88cf229567a75
tommykung <tommykung007@gmail.com>
2017-12-05 18:44:57 +0700

    // test2

--- yii2_tomtest2/views/hi/what.php
+++
@@ -1,8 +0,0 @@
-<?php $this->title = 'My Yii Application'; ?>
-<div class="site-index">
-    <div class="jumbotron">
-        <h1>What</h1>
-        <p class="lead">What Your Name</p>
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Noppadol</a></p>
-    </div>
-</div>  
